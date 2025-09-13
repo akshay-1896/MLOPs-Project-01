aba205cdb8aed910d00a9072867c785f29354c6c
Akshay Sharma <officialakshay1896@gmail.com>
2025-09-13 16:48:29 +0530

    Project structure ready, venv is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akshay Sharma",
+    author_email="officialakshay1896@gmail.com",
+    packages=find_packages()
+)
